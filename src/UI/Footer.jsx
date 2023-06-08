ea9d5a0857c5ba4fa56904f887e8a2ba249376c7
abhijitmondall <abhijitmondal.cse@gmail.com>
2023-06-09 03:25:19 +0600

    Footer Added

--- src/UI/Footer.jsx
+++ src/UI/Footer.jsx
@@ -1,10 +1,59 @@
+import { Link } from "react-router-dom";
+import "./Footer.css";
+
 const Footer = () => {
   return (
-    <footer className="my-[10rem]">
-      <div className="container">
-        <h1>Footer</h1>
-      </div>
-    </footer>
+    <>
+      <footer className="footer text-textBody text-colorGreyLight2">
+        <section className="footer__container container">
+          <div className="footer__wrap">
+            <div className="footer__info">
+              <h3 className="text-textH3 font-bold text-colorGreyLight2 pb-10">
+                MelodyMasters
+              </h3>
+
+              <p>
+                Discover captivating toy cars that fuel imagination at FunMart
+                Toys! Explore our collection for endless playtime adventures.
+                Shop now and let the excitement begin!
+              </p>
+            </div>
+            <div className="footer__info">
+              <h5>Useful Links</h5>
+
+              <span>
+                <Link to="/allToys">Shop</Link>
+              </span>
+              <span>
+                <Link to="/blogs">Blogs</Link>
+              </span>
+              <span>
+                <Link to="/register">Register</Link>
+              </span>
+              <span>
+                <Link to="/login">Login</Link>
+              </span>
+            </div>
+
+            <div className="footer__info">
+              <h5>Support</h5>
+              <span>Help Desk</span>
+              <span>Communities</span>
+              <span>Become a Partner</span>
+              <span>Developers</span>
+            </div>
+            <div className="footer__info">
+              <h5>Contact</h5>
+              <span>524 Broadway , NYC</span>
+              <span>+1 777 - 978 - 5570</span>
+            </div>
+          </div>
+          <div className="footer__text">
+            <p>@2023 FunMart Toys. All Rights Reserved</p>
+          </div>
+        </section>
+      </footer>
+    </>
   );
 };
 
