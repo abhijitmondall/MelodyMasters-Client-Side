5311b9979c86f56e25779973b13c23ec7e101e8d
abhijitmondall <abhijitmondal.cse@gmail.com>
2023-06-11 01:53:21 +0600

    Top Instructors Dynamic Data Added

--- src/UI/InstructorCard.jsx
+++ src/UI/InstructorCard.jsx
@@ -1,24 +1,28 @@
-const InstructorCard = () => {
+const InstructorCard = ({ instructorInfo }) => {
+  const { photo, name, email, students, classes } = instructorInfo;
+  console.log(instructorInfo);
+
   return (
     <>
-      <div className="card w-full glass text-textBody">
+      <div className="card w-full glass text-textBody p-[3.2rem]">
         <figure>
           <img
-            src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="car!"
+            src={photo}
+            alt={name}
+            className="w-[15rem] h-[15rem] object-cover rounded-[50%]"
           />
         </figure>
 
         <div className="text-center">
-          <h3 className="text-textH3 pt-3 px-[1.6rem]">Abhijit Mondal</h3>
-          <p className="px-[1.6rem]">abhijeetmondal5@gmail.com</p>
+          <h3 className="text-textH3 pt-3 px-[1.6rem]">{name}</h3>
+          <p className="px-[1.6rem]">{email}</p>
         </div>
 
         <div className="card-body px-0">
           <div className="card-actions border-t-[.2rem] border-colorGreyLight3 border-solid px-[1.6rem] mt-6">
             <div className="flex justify-between w-full mt-6 py-[1rem] text-center text-textH6">
-              <p className="font-bold">Students: 1000</p>
-              <p>Classes: 50</p>
+              <p className="font-bold">Students: {students}</p>
+              <p>Classes: {classes}</p>
             </div>
             <button className="btn py-[1.4rem] bg-colorPrimary text-white w-full h-auto text-textBody">
               See Classes
--- src/pages/Home/PopularInstructors.jsx
+++ src/pages/Home/PopularInstructors.jsx
@@ -1,15 +1,19 @@
 import InstructorCard from "../../UI/InstructorCard";
 import SectionTitle from "../../UI/SectionTitle";
+import useFetch from "../../hooks/useFetch";
 
 const PopularInstructors = () => {
+  const { data } = useFetch("users/top-6-instructors");
+  const { instructors } = data;
+
   return (
     <section className="my-[9rem]">
       <div className="container">
         <SectionTitle>Popular Instructors</SectionTitle>
         <div className="grid grid-cols-3 gap-[3.2rem]">
-          <InstructorCard />
-          <InstructorCard />
-          <InstructorCard />
+          {instructors?.map((instruct) => (
+            <InstructorCard key={instruct._id} instructorInfo={instruct} />
+          ))}
         </div>
       </div>
     </section>
